d180b74dd70c8b23a90698da41a15cbfd4d1bfbe
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-24 18:32:24 +0100

    create notelist model and tests for createnote and shownote functions

--- src/note-list-model.js
+++ src/note-list-model.js
@@ -1,9 +1,17 @@
 (function(exports) {
 
 function NoteList() {
-
-  
+  this._notes = [];
 }
 
+NoteList.prototype.createNote = function() {
+  note = new Note();
+  this._notes.push(note);
+};
 
-});
+NoteList.prototype.showNotes = function() {
+  return this._notes;
+};
+
+  exports.NoteList = NoteList;
+})(this);
--- src/note-tests.js
+++ src/note-tests.js
@@ -9,7 +9,6 @@ var assert = {
 };
 
 var check = {
-
   toInclude: function(container, contained) {
     if(!Array.isArray(container)) {
       throw new Error(container + ' is not an array.  Oops.');
@@ -19,7 +18,20 @@ var check = {
     } else
       throw new Error("Unlucky! " + container + " did not contain " + contained);
   }
+};
 
+var observe = {
+  toChange: function(testedFunction, array, number) {
+    initialLength = array.length;
+    testedFunction();
+    if(array.length - initialLength === number) {
+      console.log('Sweet. Your array was changed by ' + number);
+    } else if(!Array.isArray(array)) {
+      throw new Error(array + ' is not an array.  Oops.');
+    } else {
+      console.log('Dang. ' + array + 'was not changed');
+    }
+  }
 };
 
 (function(exports) {
@@ -39,10 +51,24 @@ exports.testNoteHasText = testNoteHasText;
   function testCreateNote() {
 
     var noteList = new NoteList();
-    notelist.createNote("Howdy!");
-
-
+    noteList.createNote("Howdy!");
+    assert.toEqual(noteList._notes.length, 1);
   }
 
+  exports.testCreateNote = testCreateNote;
 
-});
+})(this);
+
+(function(exports) {
+
+  function testShowNotes() {
+
+    var noteList = new NoteList();
+    noteList.createNote("Howdy!");
+    noteList.createNote("Bye now!");
+    assert.toEqual(noteList.showNotes().length, 2);
+  }
+
+  exports.testShowNotes = testShowNotes;
+
+})(this);
